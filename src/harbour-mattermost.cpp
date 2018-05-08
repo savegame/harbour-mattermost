57bdabe31b10fb58e291831b0400e4bda0dd63ac
savegame <code.codition@gmail.com>
2018-05-08 15:10:42 +0300

    + release build

--- src/harbour-mattermost.cpp
+++ src/harbour-mattermost.cpp
@@ -28,10 +28,7 @@
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef QT_QML_DEBUG
 #include <QtQuick>
-#endif
-
 #include <sailfishapp.h>
 #include "TeamsModel.h"
 #include "MattermostQt.h"
